2c3a7fbe480ad0fbf42ef30afc021debe83dd727
junka <wan.junjie@foxmail.com>
2023-08-08 17:04:28 +0800

    try fix cli for npx
    Signed-off-by: junka <wan.junjie@foxmail.com>

--- package/cli.js
+++ package/cli.js
@@ -1,9 +1,11 @@
-const ffmime = require('./index.js');
+#!/usr/bin/env node
+
+const ffmime = require("./index.js");
 
 (async function main() {
   if (process.argv.length < 3) {
-    console.log("no specified file")
-    return
+    console.log("no specified file");
+    return;
   }
   const filename = process.argv[2];
   const ret = await ffmime.GetMimeCodecs(filename);
